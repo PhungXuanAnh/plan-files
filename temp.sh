d39ff6f4c104cc8a49ba79ac48045c24815831ee
Phung Xuan Anh <phungxuananh1991@gmail.com>
2026-07-01 10:15:51 +0700

    .

--- temp.sh
+++ temp.sh
@@ -1,18 +1,1 @@
 # shellcheck disable=all
-
-there is one problem with tool call hook, agent commonly call tool in parallel, for example with codex, i saw this log:
-
-```
-PostToolUse hook (completed)
-  hook context: [planning-with-files] Update progress.md with what you just did. If a phase is now complete, update tmp/plan-with-files/4606-ui-parity/task_plan.md status. If you no longer see the planning-with-files SKILL.md rules in
-
-• PostToolUse hook (completed)
-  hook context: [planning-with-files] Update progress.md with what you just did. If a phase is now complete, update tmp/plan-with-files/4606-ui-parity/task_plan.md status. If you no longer see the planning-with-files SKILL.md rules in
-  your context (post-/compact, or you have forgotten them), reload the planning-with-files skill by yourself before continuing.
-  your context (post-/compact, or you have forgotten them), reload the planning-with-files skill by yourself before continuing.
-
-• PostToolUse hook (completed)
-  hook context: [planning-with-files] Update progress.md with what you just did. If a phase is now complete, update tmp/plan-with-files/4606-ui-parity/task_plan.md status. If you no longer see the planning-with-files SKILL.md rules in
-  your context (post-/compact, or you have forgotten them), reload the planning-with-files skill by yourself before continuing.
-
-──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
